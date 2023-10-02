500b03bd77139e8134930d80210a45941b0da93f
nestaraus <nestaraus@mail.pu>
2023-10-02 20:45:31 +0300

    Vidi metodov

---
+++ С#013/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+using System;
+// вид 1
+void Method1()
+{
+    Console.WriteLine("Автор Дмитрий");
+}
+Method1();
+
+// вид 2
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i =0;
+    while (i<count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21(msg:"Текст", count: 4);
+
+// вид 3
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+ Console.WriteLine(year);
+
+ // вид 4
+
+
+string Method4(int count, string c)
+{
+    int i =0;
+    string result =String.Empty;
+    while (i<count)
+    {
+        result=result+c;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "qwerty");
+Console.WriteLine(res);
