3314711771b05a985abb4bffe2f8cc8eb898602b
nestaraus <nestaraus@mail.pu>
2023-10-02 20:52:56 +0300

    for

---
+++ C#014/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Mime;
+string Method4(int count, string c)
+{
+    string result =String.Empty;
+   for (int i = 0; i < count; i++)
+   {
+    result = result + c;
+   }
+    return result;
+}
+string res = Method4(10, "qwerty");
+Console.WriteLine(res);
