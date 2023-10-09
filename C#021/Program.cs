a61e92977425495a414b36238e00a9ada3343df4
nestaraus <nestaraus@mail.pu>
2023-10-09 21:19:17 +0300

    zapolnyaem matrizu

---
+++ C#021/Program.cs
@@ -0,0 +1,32 @@
+﻿void PrintArray(int[,] matr)
+{
+
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray (int[,] matr)
+{
+       for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j]= new Random().Next(1,10);
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+int [,] matrix = new int[3,4];
+
+PrintArray(matrix);
+FillArray(matrix);
+PrintArray(matrix);
